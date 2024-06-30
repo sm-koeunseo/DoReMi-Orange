70b178d31f911db4ae8f77f8029260c1dea7aa2d
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-07-01 01:35:22 +0900

    Runtime Error (OutOfBounds)

--- 백준/Silver/2331. 반복수열/dfs.cpp
+++ 백준/Silver/2331. 반복수열/dfs.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int visit[236196];
@@ -14,22 +12,23 @@ void dfs(int a){
 
     visit[orig-1]++;
 
-    if (visit[orig-1]==3)
-        return;
-
     next = pow(a/1000, b) + pow((a%1000)/100, b) + pow((a%100)/10, b) + pow((a%10), b);
-
+    cout<<next;
+    if (visit[next-1]==2) return;
+        
     dfs(next);
 
-    if (visit[orig-1] == 1) cnt++;
+    if (visit[orig-1]==1) cnt++;
 }
 
 int main(){
-    int a;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
+    int a;
     cin >> a >> b;
 
     dfs(a);
-
     cout << cnt;
 }
