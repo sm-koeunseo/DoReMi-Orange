edae43238ef866219c421376141b9619ac61d330
Lita <84493719+sm-koeunseo@users.noreply.github.com>
2024-07-31 12:19:14 +0900

    [Bronze III] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int p, tmp;
+    cin >> p;
+    for (int i=0; i<9; i++){
+        cin >> tmp;
+        p -= tmp;
+    }
+
+    cout << p;
+}
