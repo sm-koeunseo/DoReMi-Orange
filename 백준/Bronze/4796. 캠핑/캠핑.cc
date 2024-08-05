138923783be2382910176a2f276406069ab7bd7e
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2024-08-05 10:12:47 +0900

    [Bronze I] Title: 캠핑, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.cc
+++ 백준/Bronze/4796. 캠핑/캠핑.cc
@@ -7,34 +7,27 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int l, p, v;
+    int l = 1, p = 1, v = 1;
     vector<int> c;
+    int t;
 
-    int case_num = 1;
-
-    while (true){
+    while (true){   // 순차 입력
         cin >> l >> p >> v;
 
-        if (l == 0 && p == 0 && v == 0) {
+        // 모두 0이면 종료
+        if (l==0 && p==0 && v==0)
             break;
-        }
 
-        int full_periods = v / p;
-        int remaining_days = v % p;
-
-        int max_days = full_periods * l;
-        if (remaining_days > l) {
-            max_days += l;
-        } else {
-            max_days += remaining_days;
-        }
-
-        c.push_back(max_days);
+        // 특정 일수 중 연속으로 다닐 수 있는 수 계산
+        t = (v/p)*l;
+        if (v%p > l)    // 나머지가 l보다 크면 l을 더함
+            t += l;
+        else
+            t += v%p;   // 나머지가 l보다 작으면 나머지 더함
+        c.push_back(t);
     }
 
-    for (int i = 0; i < c.size(); i++) {
-        cout << "Case " << (i + 1) << ": " << c[i] << endl;
-    }
+    for (int i=0; i<c.size(); i++)
+        cout << "Case " << (i+1) << ": " << c[i] << endl;
 
-    return 0;
 }
