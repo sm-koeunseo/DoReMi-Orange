db0c675779d7366193eab107b3c3bcc06780b4c2
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2025-03-06 14:59:48 +0900

    #14 [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2172 KB

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[51][51];
+int n, m, a[51][51], result = INT_MAX;
+vector<vector<int>> chickenList;
 vector<pair<int, int>> chicken, house;
-int n, m, d = INT_MAX, cd, pd, tmp;
+
+void combi (int start, vector<int> v){
+    if (v.size() == m){
+        chickenList.push_back(v);
+        return;
+    }
+    for (int i=start + 1; i < chicken.size(); i++){
+        v.push_back(i);
+        combi(i, v);
+        v.pop_back();
+    }
+    return;
+}
 
 int main(){    
     ios_base::sync_with_stdio(false);
@@ -19,20 +32,21 @@ int main(){
             else if (a[i][j] == 2) chicken.push_back(make_pair(i, j));
         }
 
-    do{
-
-        cd = 0;
-        for (int i=0; i<house.size(); i++){
-            pd = INT_MAX;
-            for (int j=0; j<m; j++){
-                pd = min(pd, abs(house[i].first - chicken[j].first) + abs(house[i].second - chicken[j].second));
+    vector<int> v;
+    combi(-1, v);
+    for (vector<int> cList : chickenList){
+        int ret = 0;
+        for (pair<int, int> home : house){
+            int _min = INT_MAX;
+            for (int ch : cList){
+                int _dis = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
+                _min = min(_min, _dis);
             }
-            cd += pd;
+            ret += _min;
         }
-        d = min(d, cd);
+        result = min(result, ret);
+    }
 
-    }while(next_permutation(chicken.begin(), chicken.end()));
-
-    cout << d;
+    cout << result;
     return 0;
 }
