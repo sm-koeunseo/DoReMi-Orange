af2e9c456e75a7e943b61a1a0114e2ecdbf853cf
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2025-03-22 01:44:35 +0900

    #11 [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 14112 KB

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[1000000], ret[1000000], n;
+stack<int> s;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    memset(ret, -1, sizeof(ret));
+    for (int i=0; i<n; i++){
+        cin >> a[i];
+        while(s.size() && a[s.top()] < a[i]) {
+            ret[s.top()] = a[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+    for (int i=0; i<n; i++) cout << ret[i] << " ";
+    
+}
