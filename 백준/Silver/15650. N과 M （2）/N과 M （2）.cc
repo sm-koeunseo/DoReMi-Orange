5db32b5a772a9d653437a169596efd739f11d0f2
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2025-04-10 19:06:26 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int nature[8];
+
+void dfs(int num, int start){
+    if (num == m){
+        for (int i=0; i<m; i++){
+            cout << nature[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i <=n; i++){
+        nature[num] = i;
+        dfs(num + 1, i + 1);
+    }
+}
+
+int main(){    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    dfs(0, 1);
+    
+    return 0;
+}
