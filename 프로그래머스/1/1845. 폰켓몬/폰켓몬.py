44c4da96eb054cabde59f8a4f69bdf66d2265255
Lita <84493719+sm-koeunseo@users.noreply.github.com>
2025-07-09 16:19:58 +0900

    [level 1] Title: 폰켓몬, Time: 0.77 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
-import collections
-
 def solution(nums):
-    coll = collections.Counter(nums)
-    num = len(nums)
-    
-    if num/2 < len(coll):
-        return num/2
-    else:
-        return len(coll)
+    return min(len(nums)/2, len(set(nums)))
