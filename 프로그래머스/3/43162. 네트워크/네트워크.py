1c39d2c9511b11c047e81e7952e60e6293d3b77a
Lita <84493719+sm-koeunseo@users.noreply.github.com>
2025-07-11 22:46:59 +0900

    [level 3] Title: 네트워크, Time: 0.64 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,16 @@
+def dfs(node, computers, visited):
+    visited[node] = True
+    for tmp in range(len(computers)):
+        if not visited[tmp] and computers[node][tmp] == 1:
+            dfs(tmp, computers, visited)
+
+def solution(n, computers):
+    visited = [False] * n
+    answer = 0
+    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, computers, visited)
+            answer += 1
+    
+    return answer
