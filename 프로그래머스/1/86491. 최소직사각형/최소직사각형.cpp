aaacc8c194c0a7ffbecf8d59093947f582b34a7d
Lita <84493719+sm-koeunseo@users.noreply.github.com>
2025-10-27 18:59:59 +0900

    [level 1] Title: 최소직사각형, Time: 0.77 ms, Memory: 4.6 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,18 +1,15 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
-int tmp, max1 = 0, max2 = 0;
+int maxl = 0, minl = 0;
 
 int solution(vector<vector<int>> sizes) {
     for (int i=0; i<sizes.size(); i++){
-        if (sizes[i][0] < sizes[i][1]){
-            tmp = sizes[i][0];
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = tmp;
-        }
-        if (sizes[i][0] > max1) max1 = sizes[i][0];
-        if (sizes[i][1] > max2) max2 = sizes[i][1];
+        maxl = max(maxl, max(sizes[i][0], sizes[i][1]));
+        minl = max(minl, min(sizes[i][0], sizes[i][1]));
     }
-    return max1 * max2;
+    return maxl * minl;
 }
